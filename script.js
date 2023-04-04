4b83e28683efe48666473e6dc188cbab2be94316
kit0-0 <111427307+kit0-0@users.noreply.github.com>
2023-04-04 17:11:05 +0530

    fixed nav transform

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 // Toggle icon navbar
 
-let menuIcon = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
+let menuIcon=document.querySelector('#menu-icon')
+let navbar=document.querySelector('.navbar')
 
-menuIcon.onclick = () => {
-  menuIcon.classList.toggle("bx-x");
-  navbar.classList.toggle("active");
-};
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')}
 
 // SKILLS
 const skill = [
