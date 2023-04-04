2d7cd29384f15f8e9f050c3908d537c0f0e34a09
kit0-0 <111427307+kit0-0@users.noreply.github.com>
2023-04-04 15:20:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Toggle icon navbar
+
+let menuIcon=document.querySelector('#menu-icon')
+let navbar=document.querySelector('.navbar')
+
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')}
